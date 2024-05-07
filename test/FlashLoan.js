8f58aae15ed8e34ee2cae4a643857e8f4deb857a
akaMiWP <bangkokappdevelopment@gmail.com>
2024-05-07 13:39:33 +0700

    Added FlashLoan solidity files and a simple test

--- test/FlashLoan.js
+++ test/FlashLoan.js
@@ -10,7 +10,39 @@ const convertWeiToEther = (wei) => {
 };
 
 describe("FlashLoan", () => {
-  beforeEach(async () => {});
+  let deployer;
+  let token;
+  let flashLoan;
 
-  describe("Deployment", async () => {});
+  beforeEach(async () => {
+    // Accounts
+    let accounts = await ethers.getSigners();
+    deployer = accounts[0];
+
+    // Contracts
+    let FlashLoan = await ethers.getContractFactory("FlashLoan");
+    let FlashLoaReceiver = await ethers.getContractFactory("FlashLoanReceiver");
+    let Token = await ethers.getContractFactory("Token");
+
+    // Deploy Token
+    token = await Token.deploy("Token X", "TKX", 1000000);
+
+    // Deploy Flash Loan Pool
+    flashLoan = await FlashLoan.deploy(await token.getAddress());
+
+    // Deposit tokens into the pool
+    await token
+      .connect(deployer)
+      .approve(await flashLoan.getAddress(), convertEtherToWei("100"));
+
+    await flashLoan.connect(deployer).depositTokens(convertEtherToWei("100"));
+  });
+
+  describe("Deployment", async () => {
+    it("expect the pool to be deposited", async () => {
+      expect(await token.balanceOf(await flashLoan.getAddress())).to.equal(
+        convertEtherToWei("100")
+      );
+    });
+  });
 });
